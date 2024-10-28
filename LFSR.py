ee2a286dda83361b50806921673ec4b46f4c79f4
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-10-28 19:08:42 +0330

    LFSR

--- LFSR.py
+++ LFSR.py
@@ -1,11 +1,32 @@
 import random
 
 class LFSR():
-    def __init__(self, degree):
+    def __init__(self, degree, taps):
         print("Initializing LFSR {} degree".format(degree))
         cur = random.randrange(0, degree, 3)
         self.register = ([0]*cur) + ([1]*(degree-cur))
         random.shuffle(self.register)
+        self.taps = taps
+
+
+    def setRegister(self, register):
+        self.register = register
 
     def getRegister(self):
         return self.register
+
+    def getTaps(self):
+        return self.taps
+
+    def shift(self, input_list):
+        cuurr_list = [[input_list[i - j] for i in range(len(input_list))] for j in range(len(input_list))]
+        return cuurr_list[1]
+
+    def successor(self):
+        tap_list = self.taps
+        tap_list.reverse()
+        tap_elements = []
+        for tl in tap_list:
+            tap_elements.append(self.register[tl])
+
+        print(tap_elements)
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from LFSR import LFSR
 
-lfsr = LFSR(10)
+lfsr = LFSR(10, [0, 3, 9])
 print(lfsr.getRegister())
+print(lfsr.getTaps())
+lfsr.successor()
+print(lfsr.shift([1,2,3,4]))
